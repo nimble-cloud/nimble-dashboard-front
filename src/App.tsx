1a871ca264e07ba2c4091776ee64a9edc978aefe
SawyerOG <oglesbysaw@gmail.com>
2024-04-09 18:35:30 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import nimbleLogo from "./assets/nimble_cloud_logo_original.webp";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={nimbleLogo} alt="Nimble logo" height="300px" />
+      </div>
+      <h1>Nimble Cloud</h1>
+      <h4>Data - Software - Operations</h4>
+      <p className="read-the-docs">Under Construction</p>
+    </>
+  );
+}
+
+export default App;
